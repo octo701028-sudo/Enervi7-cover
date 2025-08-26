e0e6f19e0438819856362205ea274ca83f7220df
octo701028-sudo <octo701028@gmail.com>
2025-08-26 14:23:55 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+
+const CACHE='enervi7-v70-core';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))) });
