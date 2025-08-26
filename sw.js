fd3c09365baf4b72f056cb205c6fd2b84a4edb63
octo701028-sudo <octo701028@gmail.com>
2025-08-26 14:47:03 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-
-const CACHE='enervi7-v70-core';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))) });
+const CACHE='e7v80';
+const ASSETS=['./','./index.html','./manifest.json','./yl_blends.json','./oracle_whispering.json','./hidden_cards.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
+self.addEventListener('activate',e=>{ e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))); });
